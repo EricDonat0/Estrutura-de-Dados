96b286ddafcc36233acc6f924e3e9a01c746a453
Eric <ericdonato09@gmail.com>
2025-04-14 20:41:17 -0300

    Atualização do exercicio 8

--- Sala de Aula/exercicio8_eric_donato.c
+++ Sala de Aula/exercicio8_eric_donato.c
@@ -1,9 +1,8 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+Implemente, em linguagem C, uma função recursiva que receba
+a raiz de uma árvore binária (não necessariamente uma árvore
+binária de busca) e retorne sua altura.
 
 *******************************************************************************/
 #include <stdio.h>
